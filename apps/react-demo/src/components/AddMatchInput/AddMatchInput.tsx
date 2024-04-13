f1b416f5a4d0b4a71984625e49e2fc6db8dfca49
wael-jaber <wael_jm@hotmail.fr>
2024-04-13 20:38:44 +0200

    feat: 🎸 Styled AddMatchInput

--- apps/react-demo/src/components/AddMatchInput/AddMatchInput.tsx
+++ apps/react-demo/src/components/AddMatchInput/AddMatchInput.tsx
@@ -58,43 +58,65 @@ export function AddMatchInput({ onAddMatch }: AddMatchInputProps): React.ReactEl
     setAwayTeamScore('');
   };
 
+  const isHomeScoreValid = Number.isInteger(Number(homeTeamScore)) && Number(homeTeamScore) >= 0;
+  const isAwayScoreValid = Number.isInteger(Number(awayTeamScore)) && Number(awayTeamScore) >= 0;
+
   return (
-    <div data-testid="add-match-input">
-      <div>
-        <label>Home Team</label>
+    <div data-testid="add-match-input" className={'flex w-full border-2 p-3 rounded-lg align-middle space-x-4 flex'}>
+      <div className={'flex items-center space-x-3'}>
+        <label className={'text-nowrap'}>Home Team</label>
         <input
+          className={
+            'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-auto py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500'
+          }
           value={homeTeamName}
+          size={10}
           data-testid={'home-team-name-input'}
           type="text"
           onChange={(e) => setHomeTeamName(e.target.value)}
         />
       </div>
-      <div>
-        <label>Away Team</label>
+      <div className={'flex items-center space-x-3'}>
+        <label className={'text-nowrap'}>Away Team</label>
         <input
+          className={
+            'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-auto py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500'
+          }
           value={awayTeamName}
+          size={10}
           data-testid={'away-team-name-input'}
           type="text"
           onChange={(e) => setAwayTeamName(e.target.value)}
         />
       </div>
-      <div>
-        <label>Home Team Score</label>
+      <div className={'flex items-center space-x-3'}>
+        <label className={'text-nowrap'}>Home Team Score</label>
         <input
+          className={`bg-gray-200 appearance-none border-2 border-gray-200 rounded w-auto py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 ${homeTeamScore !== '' && !isHomeScoreValid ? 'border-red-500' : ''}`}
           value={homeTeamScore}
+          size={10}
           data-testid={'home-team-score-input'}
           onChange={(e) => setHomeTeamScore(e.target.value)}
         />
       </div>
-      <div>
-        <label>Away Team Score</label>
+      <div className={'flex items-center space-x-3'}>
+        <label className={'text-nowrap'}>Away Team Score</label>
         <input
+          className={`bg-gray-200 appearance-none border-2 border-gray-200 rounded w-auto py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 ${awayTeamScore !== '' && !isAwayScoreValid ? 'border-red-500' : ''} `}
           value={awayTeamScore}
+          size={10}
           data-testid={'away-team-score-input'}
           onChange={(e) => setAwayTeamScore(e.target.value)}
         />
       </div>
-      <button onClick={handleAddMatch} disabled={isDisabled} data-testid="add-match-button">
+      <button
+        onClick={handleAddMatch}
+        disabled={isDisabled}
+        data-testid="add-match-button"
+        className={
+          '!ml-auto bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full disabled:opacity-50 disabled:cursor-not-allowed text-nowrap'
+        }
+      >
         Add Match
       </button>
     </div>
